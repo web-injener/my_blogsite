d09df330c141c5a2c0c850fea34d663b567bb370
web-injener <157906392+web-injener@users.noreply.github.com>
2024-08-05 14:23:16 +0900

    latest changes for deployment

--- blog/settings.py
+++ blog/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog_app'
+    'blog_app',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
 #local
-#STATIC_URL = '/static/'
-#STATIC_ROOT = BASE_DIR/'staticfiles'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR/'staticfiles'
 
 #server
 STATIC_URL = '/static/'
@@ -128,8 +130,8 @@ STATIC_ROOT = '/home/djangomo/javohir-blogs.uz/django/staticfiles'
 STATICFILES_DIRS = ('/home/djangomo/javohir-blogs.uz/django/staticfiles',)
 
 #local
-#MEDIA_URL = "/media/"
-#MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 
 #server
@@ -139,3 +141,5 @@ MEDIA_ROOT ='/home/djangomo/javohir-blogs.uz/media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
